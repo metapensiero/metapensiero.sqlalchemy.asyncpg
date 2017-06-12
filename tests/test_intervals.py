bece5d6ba265cf7e97835a0f8cd104b07e3a4359
Lele Gaifax <lele@metapensiero.it>
2017-06-12 14:28:26 +0200

    Assert that the effort was worth the hassle

--- tests/test_intervals.py
+++ tests/test_intervals.py
@@ -14,7 +14,6 @@ from metapensiero.sqlalchemy import asyncpg
 from arstecnica.ytefas.model.tables.risk import schedules
 
 
-@pytest.mark.xfail # https://github.com/MagicStack/asyncpg/issues/150
 @pytest.mark.asyncio
 async def test_schedule_repeat_after(pool):
     q = sa.select([schedules.c.repeat_after]) \
@@ -22,4 +21,4 @@ async def test_schedule_repeat_after(pool):
 
     async with pool.acquire() as conn:
         result = await asyncpg.fetchone(conn, q)
-        assert result['repeat_after'].days == 1825
+        assert result['repeat_after'].months == 12*5
