83d12b58c30d92d112c9293d1e3ad12056ee391c
Lele Gaifax <lele@metapensiero.it>
2017-12-01 13:18:01 +0100

    Emacs customization

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((fill-column . 90)
+         ;; force reinstall of whitespace font-lock customization
+         (eval . (if (functionp 'whitespace-color-on)
+                     (whitespace-color-on))))))
